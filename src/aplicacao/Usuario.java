68b65801768415ee00c8290df43d42ed7fc8b78f
Mateus Davi Simon <hlah_@hotmail.com>
2018-11-18 17:54:10 -0200

    Transferido métodos de usuário para prinicipal.

--- src/aplicacao/Usuario.java
+++ src/aplicacao/Usuario.java
@@ -26,126 +26,14 @@ public class Usuario implements Serializable {
 	}
 
 	//Adiciona uma cadeira na lista de cadeiras
-	public void adiciona_cadeira() {
-		String tempNome;
-		String tempCod;
-		int escolha;
-		List<Cadeira> prq = new ArrayList<Cadeira>();
-		
-		System.out.print("Digite o nome da cadeira: ");
-		tempNome = keyboard.nextLine();
-		System.out.print("Digite o codigo da cadeira: ");
-		tempCod = keyboard.nextLine();
-		
-		//Verifica se ja existe cadeira com o mesmo codigo
-		for(Cadeira c: this.getCadeiras()){
-			if(c.getCodigo().equals(tempCod)){
-				System.out.println("Erro: Ja existe uma cadeira com codigo " + tempCod + "! (" + c.getNome() + ")");
-				return;
-			}
-		}
-		
-		if(!this.getCadeiras().isEmpty()){
-			System.out.println("Escolha quais cadeiras sao pre requisitos da nova (0 para terminar): ");
-			
-			int i = 1;
-			for(Cadeira c : this.getCadeiras()) {
-				System.out.println(i + ") " + c.getNome());
-				i++;
-			}
-			
-			escolha = Integer.parseInt(principal.keyboard.nextLine())-1;
-			while(escolha != -1) {
-				if(escolha >= 0 && escolha < this.getCadeiras().size()) {
-					prq.add(this.getCadeiras().get(escolha));
-					System.out.println(this.getCadeiras().get(escolha).getNome() + " adicionada aos pre-requisitos");
-				}
-				escolha = Integer.parseInt(principal.keyboard.nextLine())-1;
-			}
-		}
-		
-		cadeiras.add(new Cadeira(tempNome, tempCod, prq));
+	public void adiciona_cadeira(Cadeira cadeira) {
+		cadeiras.add(cadeira);
 	}
 	
-	//Finaliza uma cadeira com uma Turma ativa.
-	//A turma eh removida da lista de Turmas ativas e a cadeira eh adicionada ao historico.
-	public void terminaCadeira(){
-		int i = 1;
-		int escolha = -1;
-		for(Turma t : this.getTurmasAtivas()) {
-			System.out.println(i + ") " + t.getCadeira().getNome());
-			i++;
-		}
-		System.out.println(i + ") Cancelar");
-		
-		while(escolha < 0 || escolha >= i) {
-			escolha = Integer.parseInt(principal.keyboard.nextLine())-1;
-		}
-		
-		if(escolha == i-1)
-			return;
-		
-		Turma turmaFinalizada = this.getTurmasAtivas().get(escolha);
-		CadeiraFinalizada cf = new CadeiraFinalizada(turmaFinalizada.getCadeira(), turmaFinalizada.calculaMedia(), turmaFinalizada.getAno(), turmaFinalizada.getSemestre());
-		this.getHistorico().addCadeiraFinalizada(cf);
-		this.getTurmasAtivas().remove(escolha);
-		
-		System.out.println("Finalizada a cadeira de " + turmaFinalizada.getCadeira().getNome() + "!");
-	}
-	
-	//Adiciona uma Turma de uma cadeira ainda nao finalizada e nao cursando a lista de Turmas ativas.
-	public void adiciona_turma() {
-		int i = 1;
-		int escolha = -1;
-		int tAno, tSem;
-		String horario;
-		String professor;
-		System.out.println("Escolha uma cadeira: ");
-		for(Cadeira c : this.getCadeiras()) {
-			System.out.println(i + ") " + c.getNome());
-			i++;
-		}
-		System.out.println(i + ") " + "Cancelar");
-		
-		while(escolha != i-1 && (escolha < 0 || escolha >= i))
-			escolha = Integer.parseInt(keyboard.nextLine())-1;
-		
-		if(escolha == i-1)
-			return;
-		
-		Cadeira c = this.getCadeiras().get(escolha);
-		
-		if(this.historico.finalizou(c)) {
-			System.out.println("Voce ja finalizou a cadeira de " + c.getNome());
-			return;
-		}
-		if(!this.getHistorico().finalizouPreReq(c)) {
-			System.out.println("Voce nao finalizou todos os pre-requisitos da cadeira " + c.getNome());
-			return;
-		}
-		if(c.isCursando()) {
-			System.out.println("Voce ja esta cursando " + c.getNome());
-			return;
-		}
-		
-		try{
-			System.out.print("Digite o Ano: ");
-			tAno = Integer.parseInt(keyboard.nextLine());
-			System.out.print("Digite o Semestre: ");
-			tSem = Integer.parseInt(keyboard.nextLine());
-			System.out.print("Digite o nome do professor: ");
-			professor = keyboard.nextLine();
-			System.out.print("Digite os horarios: ");
-			horario = keyboard.nextLine();
-		}
-		catch(Exception e){
-			System.out.println("Erro! Tente denovo...");
-			return;
-		}
-		c.setCursando(true);
-		this.getTurmasAtivas().add(new Turma(c, tAno, tSem, horario, professor));
-		System.out.println("Criada nova turma de " + c.getNome());
-		
+
+	// adiciona turma
+	public void adiciona_turma(Turma turma) {
+		this.getTurmasAtivas().add(turma);
 	}
 	
 	//Verifica se uma prova esta proxima
--- src/aplicacao/principal.java
+++ src/aplicacao/principal.java
@@ -1,6 +1,7 @@
 package aplicacao;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -97,7 +98,7 @@ public class principal {
 		usuario.getTurmasAtivas().get(escolha).listarOpcoes();
 	}
 	
-	//Lista as Provas e Trabalhos próximos (Dentro de 8 dias) e a Cadeira relacionada
+	//Lista as Provas e Trabalhos prï¿½ximos (Dentro de 8 dias) e a Cadeira relacionada
 	private static void listaAtividadesProximas(){
 		List<Prova> provasProximas = usuario.getProvasProximas();
 		List<Trabalho> trabalhosProximos = usuario.getTrabalhosProximos();
@@ -132,13 +133,13 @@ public class principal {
 		while(loop) {
 			switch(obtem_operacao()) {
 				case ADD_CADEIRA:
-					usuario.adiciona_cadeira();
+					adicionaCadeira();
 					break;
 				case ADD_TURMA:
-					usuario.adiciona_turma();
+					adicionaTurma();
 					break;
 				case TERMINA_CADEIRA:
-					usuario.terminaCadeira();
+					terminaCadeira();
 					break;
 				case LISTAR_CADEIRAS:
 					listaCadeiras();
@@ -160,6 +161,155 @@ public class principal {
 		}
 		
 		keyboard.close();
+	}
+	
+	//Adiciona uma Turma de uma cadeira ainda nao finalizada e nao cursando a lista de Turmas ativas.
+	private static void adicionaCadeira() {
+		String tempNome;
+		String tempCod;
+		int escolha;
+		List<Cadeira> prq = new ArrayList<Cadeira>();
+		
+		System.out.print("Digite o nome da cadeira: ");
+		tempNome = keyboard.nextLine();
+		System.out.print("Digite o codigo da cadeira: ");
+		tempCod = keyboard.nextLine();
+		
+		//Verifica se ja existe cadeira com o mesmo codigo
+		for(Cadeira c: usuario.getCadeiras()){
+			if(c.getCodigo().equals(tempCod)){
+				System.out.println("Erro: Ja existe uma cadeira com codigo " + tempCod + "! (" + c.getNome() + ")");
+				return;
+			}
+		}
+		
+		if(!usuario.getCadeiras().isEmpty()){
+			System.out.println("Escolha quais cadeiras sao pre requisitos da nova (0 para terminar): ");
+			
+			int i = 1;
+			for(Cadeira c : usuario.getCadeiras()) {
+				System.out.println(i + ") " + c.getNome());
+				i++;
+			}
+			
+			while(true) {
+				try {
+					escolha = Integer.parseInt(keyboard.nextLine())-1;
+					break;
+				} catch(NumberFormatException e) {
+					continue;
+				}
+			}
+			while(escolha != -1) {
+				if(escolha >= 0 && escolha < usuario.getCadeiras().size()) {
+					prq.add(usuario.getCadeiras().get(escolha));
+					System.out.println(usuario.getCadeiras().get(escolha).getNome() + " adicionada aos pre-requisitos");
+				}
+				while(true) {
+					try {
+						escolha = Integer.parseInt(keyboard.nextLine())-1;
+						break;
+					} catch(NumberFormatException e) {
+						continue;
+					}
+				}
+			}
+		}
+		
+		usuario.adiciona_cadeira(new Cadeira(tempNome, tempCod, prq));
+		
+		
+	}
+	
+	private static void adicionaTurma() {
+		int i = 1;
+		int escolha = -1;
+		int tAno, tSem;
+		String horario;
+		String professor;
+		System.out.println("Escolha uma cadeira: ");
+		for(Cadeira c : usuario.getCadeiras()) {
+			System.out.println(i + ") " + c.getNome());
+			i++;
+		}
+		System.out.println(i + ") " + "Cancelar");
+		
+		while(escolha != i-1 && (escolha < 0 || escolha >= i)) {
+			try {
+				escolha = Integer.parseInt(keyboard.nextLine())-1;
+			} catch(NumberFormatException e) {
+				continue;
+			}
+		}
+			
+		
+		if(escolha == i-1)
+			return;
+		
+		Cadeira c = usuario.getCadeiras().get(escolha);
+		
+		if(usuario.getHistorico().finalizou(c)) {
+			System.out.println("Voce ja finalizou a cadeira de " + c.getNome());
+			return;
+		}
+		if(!usuario.getHistorico().finalizouPreReq(c)) {
+			System.out.println("Voce nao finalizou todos os pre-requisitos da cadeira " + c.getNome());
+			return;
+		}
+		if(c.isCursando()) {
+			System.out.println("Voce ja esta cursando " + c.getNome());
+			return;
+		}
+		
+		try{
+			System.out.print("Digite o Ano: ");
+			tAno = Integer.parseInt(keyboard.nextLine());
+			System.out.print("Digite o Semestre: ");
+			tSem = Integer.parseInt(keyboard.nextLine());
+			System.out.print("Digite o nome do professor: ");
+			professor = keyboard.nextLine();
+			System.out.print("Digite os horarios: ");
+			horario = keyboard.nextLine();
+		}
+		catch(Exception e){
+			System.out.println("Erro! Tente denovo...");
+			return;
+		}
+		c.setCursando(true);
+		
+		usuario.adiciona_turma(new Turma(c, tAno, tSem, horario, professor));
+		
+		System.out.println("Criada nova turma de " + c.getNome());
+	}
+	
+	//Finaliza uma cadeira com uma Turma ativa.
+	//A turma eh removida da lista de Turmas ativas e a cadeira eh adicionada ao historico.
+	public static void terminaCadeira() {
+		int i = 1;
+		int escolha = -1;
+		for(Turma t : usuario.getTurmasAtivas()) {
+			System.out.println(i + ") " + t.getCadeira().getNome());
+			i++;
+		}
+		System.out.println(i + ") Cancelar");
+		
+		while(escolha < 0 || escolha >= i) {
+			try {
+				escolha = Integer.parseInt(principal.keyboard.nextLine())-1;
+			} catch(NumberFormatException e) {
+				continue;
+			}
+		}
+		
+		if(escolha == i-1)
+			return;
+		
+		Turma turmaFinalizada = usuario.getTurmasAtivas().get(escolha);
+		CadeiraFinalizada cf = new CadeiraFinalizada(turmaFinalizada.getCadeira(), turmaFinalizada.calculaMedia(), turmaFinalizada.getAno(), turmaFinalizada.getSemestre());
+		usuario.getHistorico().addCadeiraFinalizada(cf);
+		usuario.getTurmasAtivas().remove(escolha);
+		
+		System.out.println("Finalizada a cadeira de " + turmaFinalizada.getCadeira().getNome() + "!");
 	}
 
 }
